cbf63615a0e1d1a3e14428889d5df300ce0c34a1
Luciano Andrade <andrade.luciano@gmail.com>
2017-04-30 18:14:57 -0300

    second commit

---
+++ backend/mysql/statsd.config.js
@@ -0,0 +1,26 @@
+{
+	"backends" : [ "./backends/console", "/opt/nodejs-statsd-mysql-backend/mysql-backend"],
+	"server" : "./servers/tcp",
+	"address" : "0.0.0.0",
+	"port": 8125,
+	"debug" : true,
+	"deleteGauges":   true,
+	"deleteTimers":   true,
+	"deleteSets" :    true,
+	"deleteCounters": true,
+
+	mysql: { 
+	     host: "mysql", 
+	     port: 3306, 
+	     user: "statsd", 
+             password: "2uhyenoi2y0", 
+	     database: "statsd_db",
+	     backendPath : "/opt/nodejs-statsd-mysql-backend/", 
+	     engines : {
+		    counters: ["engines/countersEngine.js"],
+		    gauges:   ["engines/gaugesEngine.js"],
+		    timers:   ["engines/timersEngine.js"],
+		    sets:     ["engines/setsEngine.js"]
+	     }
+        }
+}
