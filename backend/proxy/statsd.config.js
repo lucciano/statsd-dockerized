edf083aabbd17ab053ca2e6a379af89fc18744e0
Luciano Andrade <andrade.luciano@gmail.com>
2017-05-12 12:36:56 -0300

    finally

--- backend/proxy/statsd.config.js
+++ backend/proxy/statsd.config.js
@@ -6,12 +6,8 @@
 	"address" : "0.0.0.0",
 	"port": 8125,
 	"debug" : true,
-	"deleteGauges":   true,
-	"deleteTimers":   true,
-	"deleteSets" :    true,
-	"deleteCounters": true,
-	"nodes": [
-		{host: 'backend_mysql', port: 8125},
-		{host: 'backend_mongodb', port: 8125}
-	]
+        "repeater" : [
+			{host: 'backend_mysql', port: 8125, adminport: 8126},
+			{host: 'backend_mongodb', port: 8125, adminport: 8126}
+		]
 }
